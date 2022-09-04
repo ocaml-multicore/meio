192be79bcebc6e1bd402c81a599ffb11f0dc8e4b
Patrick Ferris <patrick@sirref.org>
2022-09-04 10:31:42 +0100

    Updating to latest cohttp changes (#13)

--- src/server/main.ml
+++ src/server/main.ml
@@ -2,32 +2,32 @@ open Eio.Std
 open Cohttp_eio
 open Websocket
 
-let loader path =
+let loader path req =
   match Static.read path with
   | None ->
       traceln "%s not found" path;
-      Server.Response.not_found
+      Server.not_found_handler req
   | Some asset ->
       let mime = Magic_mime.lookup path in
       let headers =
-        Cohttp.Header.of_list
+        Http.Header.of_list
           [
             ("Content-Type", mime);
             ("Content-Length", string_of_int @@ String.length asset);
           ]
       in
-      Server.Response.create ~headers (String asset)
+      (Http.Response.make ~headers (), Cohttp_eio.Body.Fixed asset)
 
 let handle_client pid mgr send = Rev.start_trace_record mgr send pid
 
 let handler ~sw ~mgr pid : Cohttp_eio.Server.handler =
- fun request ->
+ fun ((request, _body, _stream) as req) ->
   let open Frame in
-  let uri = Cohttp_eio.Server.Request.resource request in
+  let uri = Http.Request.resource request in
   match uri with
   | "/websocket" ->
       let resp, send_frame =
-        Websocket_eio.upgrade_connection request (fun { opcode; content; _ } ->
+        Websocket_eio.upgrade_connection req (fun { opcode; content; _ } ->
             match opcode with
             | Opcode.Close -> traceln "[RECV] CLOSE"
             | _ -> traceln "[RECV] %s" content)
@@ -38,13 +38,13 @@ let handler ~sw ~mgr pid : Cohttp_eio.Server.handler =
       in
       Fiber.fork ~sw go;
       resp
-  | "/" | "/index.html" -> loader "index.html"
-  | path -> loader path
+  | "/" | "/index.html" -> loader "index.html" req
+  | path -> loader path req
 
 let start_server env sw pid port =
   traceln "[SERV] listening on port %d" port;
   let mgr = Eio.Stdenv.domain_mgr env in
-  Cohttp_eio.Server.run ~port env sw (handler ~sw ~mgr pid)
+  Cohttp_eio.Server.run ~port env (handler ~sw ~mgr pid)
 
 let () =
   let pid =
