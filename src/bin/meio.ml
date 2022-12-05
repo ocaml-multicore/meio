e3104e53d4ebc99285348a7854d4de121529376a
Patrick Ferris <patrick@sirref.org>
2022-12-05 09:39:09 +0000

    Use Unix.create_process_env (#20)
    * Use Unix.create_process_env
    * Remove eio_luv from opam file
    * Update to latest runtime events
    * Kill leaked process and use Eio_main

--- src/bin/meio.ml
+++ src/bin/meio.ml
@@ -1,28 +1,28 @@
-module Process = Eio_luv.Low_level.Process
-
 let run _stdenv exec_args =
   let argsl = String.split_on_char ' ' exec_args in
   let executable_filename = List.hd argsl in
+  let argsl = Array.of_list argsl in
   let tmp_dir = Filename.get_temp_dir_name () in
-  let pair = function x :: [ y ] -> (x, y) | _ -> assert false in
   let env =
     Array.append
       [|
-        ("OCAML_RUNTIME_EVENTS_START", "1");
-        ("OCAML_RUNTIME_EVENTS_DIR", tmp_dir);
-        ("OCAML_RUNTIME_EVENTS_PRESERVE", "1");
+        "OCAML_RUNTIME_EVENTS_START=1";
+        "OCAML_RUNTIME_EVENTS_DIR=" ^ tmp_dir;
+        "OCAML_RUNTIME_EVENTS_PRESERVE=1";
       |]
-      (Unix.environment ()
-      |> Array.map (fun s -> String.split_on_char '=' s |> pair))
-    |> Array.to_list
+      (Unix.environment ())
   in
-  let child_pid = Process.spawn ~env executable_filename argsl in
+  let child_pid =
+    Unix.create_process_env executable_filename argsl env Unix.stdin Unix.stdout
+      Unix.stderr
+  in
   Unix.sleepf 0.2;
-  let handle = (tmp_dir, Process.pid child_pid) in
+  let handle = (tmp_dir, child_pid) in
   Meio.ui handle;
+  Unix.kill child_pid Sys.sigkill;
   let ring_file =
-    Filename.concat tmp_dir (string_of_int (Process.pid child_pid) ^ ".events")
+    Filename.concat tmp_dir (string_of_int child_pid ^ ".events")
   in
   Unix.unlink ring_file
 
-let () = Eio_luv.run @@ fun stdenv -> run stdenv Sys.argv.(1)
+let () = Eio_main.run @@ fun stdenv -> run stdenv Sys.argv.(1)
