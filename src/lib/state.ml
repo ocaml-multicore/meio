09db5d9c5a36478bf7e7afba67d30f9dbe519465
Lucas Pluvinage <lucas@tarides.com>
2023-03-30 10:56:35 +0200

    ocamlformat 0.25.1

--- src/lib/state.ml
+++ src/lib/state.ml
@@ -3,7 +3,9 @@ let tasks = Task_table.create Sort.Tree
 let set_sort_mode = Task_table.set_sort_mode tasks
 
 let add_tasks (id, parent_id, domain, ts) =
-  let task = Task.create ~id ~domain ~parent_id (Runtime_events.Timestamp.to_int64 ts) in
+  let task =
+    Task.create ~id ~domain ~parent_id (Runtime_events.Timestamp.to_int64 ts)
+  in
   Task_table.add tasks task
 
 let remove_task i = Task_table.remove_by_id tasks i
