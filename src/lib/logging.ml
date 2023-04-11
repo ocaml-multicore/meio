6931b2000ed42cae6b6c28908d3e4f43d7481d7b
Lucas Pluvinage <lucas@tarides.com>
2023-04-11 14:32:41 +0200

    logging: lwd is not domain safe

--- src/lib/logging.ml
+++ src/lib/logging.ml
@@ -1,14 +1,28 @@
+module Queue = Eio_utils.Lf_queue
+
 let table = Lwd_table.make ()
-let buf = Buffer.create 200
-let log_fmt = Format.formatter_of_buffer buf
+let waiting = Queue.create ()
+
+let buf_fmt_key =
+  Domain.DLS.new_key (fun () ->
+      let buf = Buffer.create 2000 in
+      (buf, Format.formatter_of_buffer buf))
+
+let mtx = Mutex.create ()
+
+let () =
+  Logs.set_reporter_mutex
+    ~lock:(fun () -> Mutex.lock mtx)
+    ~unlock:(fun () -> Mutex.unlock mtx)
 
 let reporter () =
   let report src level ~over k msgf =
+    let buf, log_fmt = Domain.DLS.get buf_fmt_key in
     let k _ =
       Format.pp_print_flush log_fmt ();
       let msg = Buffer.contents buf in
       Buffer.clear buf;
-      Lwd_table.append' table msg;
+      Queue.push waiting msg;
       over ();
       k ()
     in
@@ -16,3 +30,10 @@ let reporter () =
     msgf @@ fun ?header ?tags fmt -> log header tags k fmt
   in
   { Logs.report }
+
+let rec poll () =
+  match Queue.pop waiting with
+  | Some msg ->
+      Lwd_table.prepend' table msg;
+      poll ()
+  | None -> ()
